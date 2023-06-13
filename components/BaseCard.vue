6da4eafb7ceb6a37ce909d0058c502904182f04b
akhfih <akhfih.dev@gmail.com>
2023-06-13 16:50:38 +0700

    feat: add BaseCard component

---
+++ components/BaseCard.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <b-card
+      :title="title"
+      :img-src="image"
+      img-alt="Image"
+      img-top
+      tag="div"
+      class="custom-card"
+      @click="clickHandler"
+    >
+      <b-card-text> {{ text }} </b-card-text>
+    </b-card>
+  </div>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+export default {
+  name: 'BaseCard',
+  props: {
+    image: { type: String, default: '' },
+    title: { type: String, default: '' },
+    text: { type: String, default: '' },
+  },
+  emits: ['clickHandler'],
+  setup(props, { emit }) {
+    const state = reactive({})
+
+    const clickHandler = () => {
+      emit('clickHandler')
+    }
+
+    return {
+      ...toRefs(state),
+      clickHandler,
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: 12px;
+  padding: 0.5rem !important;
+  max-width: 176px;
+  max-height: 223px;
+  box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  border: 1px solid #f9fafb;
+
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0px 20px 25px -5px rgba(16, 24, 40, 0.1),
+      0px 8px 10px -6px rgba(16, 24, 40, 0.1);
+  }
+  img {
+    border-radius: 6px;
+    max-width: 160px;
+    max-height: 153px;
+    // object-fit: contain;
+    object-fit: cover;
+    border: 1px solid #f9fafb;
+  }
+
+  &-body {
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  &-title {
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 24px;
+
+    display: flex;
+    align-items: center;
+    text-align: center;
+    font-feature-settings: 'salt' on;
+
+    color: #111827;
+    margin-bottom: 0;
+  }
+  &-text {
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 20px;
+    color: #6b7280;
+  }
+}
+</style>
--- components/Home.vue
+++ components/Home.vue
@@ -8,11 +8,33 @@
         >Siapa yang mencintaiku akan mencintai anjingku juga.</template
       >
     </BaseHeading>
+    <base-card
+      image="https://images.dog.ceo/breeds/hound-afghan/n02088094_3937.jpg"
+      title="Terries"
+      text="123 sub-breeds"
+      @clickHandler="tes"
+    ></base-card>
+
+    <b-modal v-model="modalShow">Hello From Modal!</b-modal>
   </div>
 </template>
 
 <script>
+import { reactive, toRefs } from 'vue'
 export default {
   name: 'NuxtHome',
+  setup() {
+    const state = reactive({
+      modalShow: false,
+    })
+    function tes() {
+      state.modalShow = !state.modalShow
+    }
+
+    return {
+      ...toRefs(state),
+      tes,
+    }
+  },
 }
 </script>
